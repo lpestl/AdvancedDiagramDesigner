bb1e3eed99f37f5b7d51fec9f90f043bd1058306
Mikhail S. Kataev <lpestlname@gmail.com>
2019-05-12 18:27:20 +0300

    Added correct closing tabs

--- DiagramDesigner/Views/MainWindow.xaml.cs
+++ DiagramDesigner/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using DiagramDesigner.Functionality;
@@ -7,20 +10,37 @@ namespace DiagramDesigner.Views
 {
     public partial class MainWindow : Window
     {
+        private ObservableCollection<DiagramTabItem> diagramTabs { get; set; } = new ObservableCollection<DiagramTabItem>();
         public MainWindow()
         {
             InitializeComponent();
 
-            DesignerCanvasesHolder.Items.Add(new DiagramTabItem
-            {
-                Header = Properties.Resources.NewDiagram,
-                Content = new DiagramControl()
-            });
+            diagramTabs.CollectionChanged += DiagramTabsOnCollectionChanged;
+
+            DesignerCanvasesHolder.ItemsSource = diagramTabs;
+
+            diagramTabs.Add(new DiagramTabItem
+                {
+                    Header = Properties.Resources.NewDiagram,
+                    Content = new DiagramControl()
+                });
         }
 
-        private void CloseDiagram_MouseDown(object sender, MouseButtonEventArgs e)
+        private void DiagramTabsOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            SetVisibilityTabItemHeaders(diagramTabs.Count > 1 ? Visibility.Visible : Visibility.Collapsed);
+        }
+
+        private void SetVisibilityTabItemHeaders(Visibility visibility)
+        {
+            Style s = new Style();
+            s.Setters.Add(new Setter(UIElement.VisibilityProperty, visibility));
+            DesignerCanvasesHolder.ItemContainerStyle = s;
+        }
+
+        private void CloseDiagram_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            DesignerCanvasesHolder.Items.RemoveAt(DesignerCanvasesHolder.SelectedIndex);
         }
     }
 }
